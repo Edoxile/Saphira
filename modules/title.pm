951a60329bef9a2b41563a4a4a9012980ec355b1
Edoxile <koen_75_@hotmail.com>
2013-03-19 23:18:40 +0100

    Fixed namespace and added Title module

--- modules/title.pm
+++ modules/title.pm
@@ -19,32 +19,30 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 =end comment
 =cut
 
-package Saphira::Module::Admin;
+package Saphira::Module::Title;
 use base 'Saphira::Module';
 
 sub init {
     my ( $self, $message, $args ) = @_;
-
-    $self->registerHook( 'said', \&handleSaidKick );
-
-    #$self->registerHook('said', \&handleSaidInvite);
+    
+    $self->registerHook('said', \&handleSaid);
 }
 
-sub getAuthLevel {
-    my ( $server, $message ) = @_;
-    my $user = $server->getUser( $message->{raw_nick} );
-    return 0 unless defined $user;
-    return 9 if $user->isOperator();
-    return 6 if $user->isChannelOperator();
-    return $user->getPermission( $message->{channel} );
-}
-
-sub handleSaidKick {
-    my ( $wrapper, $server, $message ) = @_;
-
-    return unless ( $message->{body} =~ m/^!kick\s(.+?)(?:\s(.+?))$/ );
-
-    $server->getChannel( $message->{channel} )->kick( $1, $2 );
+sub handleSaid {
+    my ($bot, $message) = @_;
+    
+    return unless ($message->{body} =~ /((?:https?:\/\/|www\.)[-~=\\\/a-zA-Z0-9\.:_\?&%,#\+]+)/);
+    return if ($1 eq '');
+    
+    my $title = title($1);
+    return unless defined($title);
+    
+    $bot->say(
+        who     => $message->{who},
+        channel => $message->{channel},
+        body    => "[ $title ]",
+        address => $message->{address}
+    );
 }
 
 1;
